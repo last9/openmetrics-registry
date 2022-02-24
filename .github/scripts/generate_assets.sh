e89c82e71a48028efd8f7454e2cedc58af445f93
Arjun Mahishi <arjun.mahishi@gmail.com>
2022-02-24 16:41:27 +0530

    [gh-action] Checkout code with fetch-depth as 0
    - Fetching with fetch-depth as 1 (default) will not fetch the previous
      tags
    - Fetch depth 0 pull all the previous tags. We need this to compose a
      list of "changed" files since last release

--- .github/scripts/generate_assets.sh
+++ .github/scripts/generate_assets.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 # This script's responsibility is to create a list of modules to be uploaded as
 # release assets. To achieve that, it takes the following steps:
 #
-#   1. Get a list of HCL files that have changed from the last release -- by
-#   taking a diff from the HEAD commit to the last created release tag
+#   1. Get a list of HCL files to release
+#     A. If this is a module release, then only take the list of modules that
+#     have changed since he last release
+#     B. If this is a major release, then take all the modules (the first
+#     agruement is passed as "major")
 #
 #   2. Once we have that list, we need to create a mapping of those file paths
 #   to the names that they will be released as
@@ -12,16 +17,24 @@
 #   3. This mapping will be fed to the github action that will upload the
 #   assets with the specified names
 
-LATEST_RELEASE_TAG=$(git tag --list --sort creatordate | grep "module-\|release-" | tail -n1)
-git diff --name-only HEAD..$LATEST_RELEASE_TAG |\
-  grep -v "^sample\/" | grep "\.hcl" |\
-  awk '{
-    a=$1; b=$1; path=$1
-    gsub("/", "_", a)
-    n=split($1, s, "/")
-    gsub(s[n], "version.txt", b)
-    cmd = "cat " b
-    cmd | getline c
-    gsub("[[:alnum:]]+[[:punct:]]hcl", "v"c".hcl", a)
-    print a":"path
-  }'
+list_modules() {
+  if [[ "$1" == "major" ]]; then
+    find aws/ gcp/ -name '*.hcl'
+  else
+    LATEST_RELEASE_TAG=$(git tag --list --sort creatordate | grep "module-\|release-" | tail -n2 | head -n1)
+    git diff --name-only HEAD..$LATEST_RELEASE_TAG | grep -v "^sample\/" | grep "\.hcl"
+  fi
+}
+
+list_modules $1 | awk '{
+  a=$1; b=$1; path=$1
+  gsub("[[:punct:]]/", "", a);
+  gsub("[[:punct:]]/", "", path);
+  gsub("/", "_", a)
+  n=split($1, s, "/")
+  gsub(s[n], "version.txt", b)
+  cmd = "cat " b
+  cmd | getline c
+  gsub("[[:alnum:]]+[[:punct:]]hcl", "v"c".hcl", a)
+  print a":"path
+}'
