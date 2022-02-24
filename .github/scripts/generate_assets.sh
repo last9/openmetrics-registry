4fd07b362d9b4bbbb2309d94f878bb57bf82af10
Arjun Mahishi <arjun.mahishi@gmail.com>
2022-02-24 12:23:14 +0530

    [gh-action] Update generate_assets.sh script
    - Earlier, all the modules were being updated as release assets on every release
    - With this commit, only the modules that were changed since the last
      release will be uploaded

--- .github/scripts/generate_assets.sh
+++ .github/scripts/generate_assets.sh
@@ -1,7 +1,21 @@
-module_dirs="aws gcp"
+# This script's responsibility is to create a list of modules to be uploaded as
+# release assets. To achieve that, it takes the following steps:
+#
+#   1. Get a list of HCL files that have changed from the last release -- by
+#   taking a diff from the HEAD commit to the last created release tag
+#
+#   2. Once we have that list, we need to create a mapping of those file paths
+#   to the names that they will be released as
+#   Format: <release asset name>:<file path>
+#   Example: aws_cloudwatch_rds_v0.0.1.hcl:aws/cloudwatch/rds/rds.hcl
+#
+#   3. This mapping will be fed to the github action that will upload the
+#   assets with the specified names
 
-for d in $module_dirs; do
-  find $d -name "*.hcl" | awk '{
+LATEST_RELEASE_TAG=$(git tag --list --sort creatordate | grep "module-\|release-" | tail -n1)
+git diff --name-only HEAD..$LATEST_RELEASE_TAG |\
+  grep -v "^sample\/" | grep "\.hcl" |\
+  awk '{
     a=$1; b=$1; path=$1
     gsub("[[:punct:]]/", "", a);
     gsub("[[:punct:]]/", "", path);
@@ -13,4 +27,3 @@ for d in $module_dirs; do
     gsub("[[:alnum:]]+[[:punct:]]hcl", "v"c".hcl", a)
     print a":"path
   }'
-done
