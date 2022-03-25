eb2de73d26f0a56e7755576c3410150751541dd7
Arjun Mahishi <arjun.mahishi@gmail.com>
2022-03-25 13:02:57 +0530

    [gh-action] Change the binary version in the Makefile for running tests

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,12 +1,18 @@
-grep -r "scraper.*module" ../ | grep -v "/tests" | awk '{print $2}' | sort > /tmp/expected_modules
-grep -r "extends.*{" . | grep -v "/tests" | awk '{print $2}' | sort > /tmp/actual_modules
+grep -r "^scraper.*module" ../ | grep -v "/tests" | awk '{print $2}' | sort > /tmp/expected_modules
+grep -r "^extends.*{" . | grep -v "/tests" | awk '{print $2}' | sort > /tmp/actual_modules
 
 expected_modules_count=$(cat /tmp/expected_modules | wc -l)
 actual_modules_count=$(cat /tmp/actual_modules | wc -l)
 
+echo "Test report:"
+echo "Number of modules: $expected_modules_count"
+echo "Number of test extend blocks: $actual_modules_count\n"
+
 if [ "$expected_modules_count" -ne "$actual_modules_count" ]; then
-  echo "There is a mismatch in the number of test extend blocks and actual modules"
-  echo "If you have added a new module, make sure you also add a test extend block for it in tests/"
-  diff /tmp/expected_modules /tmp/actual_modules
+  echo "💥 There is a mismatch in the number of test extend blocks and actual modules"
+  echo "If you have added a new module, make sure you also add a test extend block for it in tests/\n\nDiff:\n"
+  diff -y --suppress-common-lines /tmp/expected_modules /tmp/actual_modules
   exit 1
 fi
+
+echo "All good 👍"
